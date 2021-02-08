f0c000da8b6e45c29dac7c5698d7695e49d61248
nmacchitella <44071046+nmacchitella@users.noreply.github.com>
2021-02-08 10:11:30 -0500

    o la va o la spacca4

--- giaggi/settings.py
+++ giaggi/settings.py
@@ -19,15 +19,33 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@=#7ld6fczj-cj)$&qm(5%$y&n0o7-8j2ixkbsnyj(d*r4z=)o'
+# heroku config:set DJANGO_SECRET_KEY="@=#7ld6fczj-cj)$&qm(5%$y&n0o7-8j2ixkbsnyj(d*r4z=)o"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@=#7ld6fczj-cj)$&qm(5%$y&n0o7-8j2ixkbsnyj(d*r4z=)o')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
