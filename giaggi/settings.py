4cb592106ff1692139c9bdd0ac11168d2bfb5a7a
nmacchitella <44071046+nmacchitella@users.noreply.github.com>
2021-02-21 10:17:59 -0800

    add heroku

--- giaggi/settings.py
+++ giaggi/settings.py
@@ -143,5 +143,5 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
 # Configure Django App for Heroku.
-#import django_on_heroku
-#django_on_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
