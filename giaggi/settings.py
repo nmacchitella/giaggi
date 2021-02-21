2a45a4f29aa05f40a68f3363be09f94e4ad7a694
nmacchitella <44071046+nmacchitella@users.noreply.github.com>
2021-02-21 12:23:26 -0800

    production false

--- giaggi/settings.py
+++ giaggi/settings.py
@@ -31,10 +31,12 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('DJANGO_DEBUG')=='False':
-    DEBUG = False
-else:
-    DEBUG = True
+#if os.environ.get('DJANGO_DEBUG')=='True':
+#    DEBUG = True
+#else:
+#    DEBUG = False
+
+DEBUG=False
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'giaggi.herokuapp.com']
